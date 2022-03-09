809ac95d9e204d0fa514327fa845bbf6f3711db9
frogfather <j.a.campbell@icloud.com>
2022-03-09 20:03:42 +0000

    Initial Commit

---
+++ Winos 2023/Winos_2023App.swift
@@ -0,0 +1,17 @@
+//
+//  Winos_2023App.swift
+//  Winos 2023
+//
+//  Created by Cloudsoft on 09/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct Winos_2023App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
