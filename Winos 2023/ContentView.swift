8beaca4fc377dc4c54cf1b407657ccc35e053ad9
frogfather <j.a.campbell@icloud.com>
2022-03-10 20:10:58 +0000

    Mess around with lazy v stack

--- Winos 2023/ContentView.swift
+++ Winos 2023/ContentView.swift
@@ -10,15 +10,16 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         LazyVStack(alignment: .leading) {
+            Text("Hi")
             ForEach(1...10, id: \.self) { count in
-                Label("Wine \(count)", systemImage: /*@START_MENU_TOKEN@*/"42.circle"/*@END_MENU_TOKEN@*/)
+                Label("Wine and some other text \(count)", systemImage: /*@START_MENU_TOKEN@*/"42.circle"/*@END_MENU_TOKEN@*/)
+                    .padding(6.0).background(Color.yellow)
                 
             }
-        }.background(Color.brown)
+        }.padding(10.0)
+            .background(Color.gray)
     }
 }
-
-
 
 
 
