0bed5db1b8a23de0c0317ba747dd16cf4325f38d
frogfather <j.a.campbell@icloud.com>
2022-03-11 07:22:58 +0000

    Buttons and spacers

--- Winos 2023/ContentView.swift
+++ Winos 2023/ContentView.swift
@@ -9,13 +9,35 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        LazyVStack(alignment: .leading) {
-            Text("Hi")
+        VStack(alignment: .leading) {
             ForEach(1...10, id: \.self) { count in
                 Label("Wine and some other text \(count)", systemImage: /*@START_MENU_TOKEN@*/"42.circle"/*@END_MENU_TOKEN@*/)
                     .padding(6.0).background(Color.yellow)
                 
             }
+            HStack{
+                Button(
+                    action: {},
+                    label: {
+                        Text("Button 1")
+                            .fontWeight(.bold)
+                            .foregroundColor(Color.white)
+                        
+                        
+                    }
+                ).background(Color.black)
+                Spacer()
+                Button(
+                    action: {},
+                    label: {
+                        Text("Button 2")
+                            .fontWeight(.bold)
+                            .foregroundColor(Color.white)
+                        
+                    }
+                ).background(Color.black)
+            }.padding(.horizontal)
+                .background(Color.blue)
         }.padding(10.0)
             .background(Color.gray)
     }
